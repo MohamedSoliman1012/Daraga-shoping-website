0c164a8adb10614f95fda6eb18bebe3f5d8d3c09
Mahmoudd-Elgendy <mahmoud.ayman.elgendy@gmail.com>
2025-11-30 20:51:12 +0200

    num1

--- js/AdminScript.js
+++ js/AdminScript.js
@@ -1,7 +1,43 @@
-function logout(){
-            checklogout = confirm('Are you sure you want to log out?');
-             if (checklogout) {
-                window.location.href = '../user-validation/index.html';
-            }
+// --- Authentication Functions ---
+function logout() {
+    
+    let checklogout = confirm('Are you sure you want to log out?');
 
+    if (checklogout) {
+        window.location.href = '../user-validation/index.html';
+    }
+}
+
+// --- Deletion Functions ---
+function deluser() {
+
+    confirm('Delete this user?');
+}
+
+function delproduct() {
+
+    confirm('Delete this user?');
+}
+
+// --- Product Management Functions ---
+function addproduct() {
+
+    alert("product addeed completed");
+}
+
+function addedproduct() {
+
+    window.location.href = 'adminProducts.html#Added-product';
+}
+
+// --- Navigation Functions ---
+
+function userspage() {
+    // Redirect to the User Administration page
+    window.location.href = 'adminUsers.html';
+}
+
+function orderspage() {
+    // Redirect to the Order Administration page
+    window.location.href = 'adminOrders.html';
 }
--- js/navigation.js
+++ js/navigation.js
@@ -1,45 +1,61 @@
+// --- Authentication & User Flow ---
+
 function login(event) {
     event.preventDefault();
 
-    var userRadio = document.getElementById("user");
-    var adminRadio = document.getElementById("admin");
+    const userRadio = document.getElementById("user");
+    const adminRadio = document.getElementById("admin");
 
     if (userRadio.checked) {
-               window.location.href = '../user-panel/home.html';
-
+        window.location.href = '../user-panel/home.html';
     } 
     else if (adminRadio.checked) {
         window.location.href = '../admin-panel/adminHome.html';
     } 
     else {
-    
+
         alert("Please select a login type!");
     }
 }
 
+function logout() {
 
-
-
-function logout(){
-            checklogout = confirm('Are you sure you want to log out?');
-             if (checklogout) {
-                window.location.href = '../user-validation/index.html';
-            }
+    let checklogout = confirm('Are you sure you want to log out?');
+    
+    if (checklogout) {
+        window.location.href = '../user-validation/index.html';
+    }
 }
 
-function continueBtn(){
-    window.location.href = '../user-panel/home.html';
+// --- Navigation Buttons ---
 
+function bicyclespage() {
+    window.location.href = 'bicycles.html';
 }
-function checkout(){
-    window.location.href = '../user-panel/checkout.html';
-
+function repairpage() {
+    window.location.href = 'repair.html';
+}
+function accessoriespage() {
+    window.location.href = 'accessories.html';
+}
+function continueBtn() {
+    window.location.href = 'home.html';
 }
 
-
-function buynow(){
+function checkout() {
     window.location.href = 'checkout.html';
 }
-function addtocart(){
+
+function itempage() {
+    window.location.href = 'itempage.html';
+}
+
+// --- Shopping Actions ---
+
+function buynow() {
+    window.location.href = 'checkout.html';
+}
+
+function addtocart() {
     window.location.href = 'shopping-cart.html';
 }
