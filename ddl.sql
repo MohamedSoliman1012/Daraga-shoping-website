f7a0d1f2508b9e71440679a4ea8088887f91e0c4
Mohamed soliman <66283325+MohamedSoliman1012@users.noreply.github.com>
2025-12-03 21:44:22 +0200

    dll

---
+++ ddl.sql
@@ -0,0 +1,43 @@
+
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+);
+
+
+CREATE TABLE admins (
+    email VARCHAR(150) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+)
+
+
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    details TEXT,
+    image_url VARCHAR(255),
+    category VARCHAR(99999)
+    
+);
+
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    full_name VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    payment_method VARCHAR(50) NOT NULL,
+    status VARCHAR(50) DEFAULT 'pending',
+    created_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+
